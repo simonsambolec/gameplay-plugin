1168535e59f3c3f67295f85dc56d2eeeac9b2227
simonsambolec <simon.sambolec@student.um.si>
2023-02-20 20:03:45 +0100

    call github api for videos list

--- js/background.js
+++ js/background.js
@@ -1,13 +1,37 @@
+var videos = {};
 
 /**
- * Listen for tab updates
+ * 
  */
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+async function main() {
+    addTabUpdateListener();
+    videos = await httpGet("https://raw.githubusercontent.com/simonsambolec/gameplay-plugin/main/assets/videos.json");
+    console.log(videos)
+}
 
-    if (!changeInfo.url) return;
+/**
+ * Listener for tab updates
+ */
+function addTabUpdateListener() {
+    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 
-    if (changeInfo.url.includes("www.youtube.com")) {
-        chrome.tabs.sendMessage(tab.id, "hello from background")
-    }
-});
+        if (!changeInfo.url) return;
 
+        if (changeInfo.url.includes("www.youtube.com")) {
+            chrome.tabs.sendMessage(tab.id, "hello from background")
+        }
+    });
+}
+
+/**
+ * 
+ * @param {string} url 
+ * @returns 
+ */
+async function httpGet(url) {
+    let response = await fetch(url)
+        .then((response) => response.json())
+    return response;
+}
+
+main();
